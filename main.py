fa45782e9df178958fc46c8bc88766dc6fd9a5f4
miracleexotic <mai_nutthawat@hotmail.com>
2025-10-06 20:51:15 +0700

    Add: file integration

--- main.py
+++ main.py
@@ -10,15 +10,16 @@ if __name__ == "__main__":
     transport = Transport(config["BEARER_TOKEN"])
     wash = Wash(config["BEARER_TOKEN"])
 
-    start_date = "2025-06-01"
-    end_date = "2025-06-30"
+    start_date = "2025-09-01"
+    end_date = "2025-09-30"
+    file_ttb = "data/AccountStatement_30092025.pdf"
 
-    # food.create_by_date(start_date, end_date)
-    # transport.create_by_date(start_date, end_date)
+    # food.create_by_date(start_date, end_date, file_ttb)
+    # transport.create_by_date(start_date, end_date, file_ttb)
     # wash.create_by_date(start_date, end_date)
 
-    food.update_by_date(start_date, end_date)
-    transport.update_by_date(start_date, end_date)
+    # food.update_by_date(start_date, end_date)
+    # transport.update_by_date(start_date, end_date)
 
     # month = Month(config['BEARER_TOKEN'])
     # month.get_by_date("2024-09-01")
