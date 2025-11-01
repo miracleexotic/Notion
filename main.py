d81c2e2679a25154520211ef5622e7ea195e210e
miracleexotic <mai_nutthawat@hotmail.com>
2025-11-02 02:20:38 +0700

    Edit: comment workflow

--- main.py
+++ main.py
@@ -10,16 +10,28 @@ if __name__ == "__main__":
     transport = Transport(config["BEARER_TOKEN"])
     wash = Wash(config["BEARER_TOKEN"])
 
-    start_date = "2025-09-01"
-    end_date = "2025-09-30"
-    file_ttb = "data/AccountStatement_30092025.pdf"
+    start_date = "2025-10-01"
+    end_date = "2025-10-31"
+    file_ttb = "data/AccountStatement_01112025.pdf"
+
+    # ------ #
+    # CREATE #
+    # ------ #
 
     # food.create_by_date(start_date, end_date, file_ttb)
     # transport.create_by_date(start_date, end_date, file_ttb)
     # wash.create_by_date(start_date, end_date)
 
+    # ------ #
+    # UPDATE #
+    # ------ #
+
     # food.update_by_date(start_date, end_date)
     # transport.update_by_date(start_date, end_date)
+
+    # ---- #
+    # TEST #
+    # ---- #
 
     # month = Month(config['BEARER_TOKEN'])
     # month.get_by_date("2024-09-01")
